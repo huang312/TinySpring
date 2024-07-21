3a3505119085820e26f0a367f00ed4f3d9cd582f
jianbo <1784749609@qq.com>
2024-07-21 18:27:44 +0800

    实现bean 的后处理功能

--- src/main/java/com/tiny/springframework/bean/factory/config/AutowireCapableBeanFactory.java
+++ src/main/java/com/tiny/springframework/bean/factory/config/AutowireCapableBeanFactory.java
@@ -1,8 +1,30 @@
 package com.tiny.springframework.bean.factory.config;
 
+import com.tiny.springframework.bean.exception.BeansException;
+import com.tiny.springframework.bean.factory.BeanFactory;
+
 /**
  * 自动化处理Bean工厂配置的接口
  */
-public interface AutowireCapableBeanFactory {
+public interface AutowireCapableBeanFactory extends BeanFactory {
+    /**
+     * 执行Bean实例化的前置处理
+     *
+     * @param existingBean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName) throws BeansException;
+
+    /**
+     * 执行Bean实例化的后置处理
+     *
+     * @param existingBean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName) throws BeansException;
 
 }
--- src/main/java/com/tiny/springframework/bean/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/tiny/springframework/bean/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,22 +1,31 @@
 package com.tiny.springframework.bean.factory.support;
 
+import cn.hutool.core.util.StrUtil;
 import com.tiny.springframework.bean.PropertyValue;
 import com.tiny.springframework.bean.PropertyValues;
+import com.tiny.springframework.bean.factory.InitializingBean;
+import com.tiny.springframework.bean.factory.config.AutowireCapableBeanFactory;
 import com.tiny.springframework.bean.factory.config.BeanDefinition;
+import com.tiny.springframework.bean.factory.config.BeanPostProcessor;
 import com.tiny.springframework.bean.factory.config.BeanReference;
 import com.tiny.springframework.bean.exception.BeansException;
 import com.tiny.springframework.bean.factory.utils.BeanUtil;
 
+import java.lang.reflect.Method;
+
 /**
  * 实现 Bean 实例的创建方法
  */
-public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory implements AutowireCapableBeanFactory {
     private InstantiationStrategy instantiationStrategy = new ReflectionInstantiationStrategy();
 
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException{
+        // 创建Bean实例
         Object bean = instantiationStrategy.instantiate(beanDefinition, beanName, args);
+        // 给 Bean 填充属性
         propertyInjection(beanDefinition, bean);
+        // 执行bean的初始化方法和 BeanPostProcessor 的前置和后置处理方法
         addSingleton(beanName, bean);
         return bean;
     }
@@ -37,4 +46,74 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         }
     }
 
+    /**
+     * 初始化bean
+     *
+     * @param beanName
+     * @param bean
+     * @param beanDefinition
+     * @return
+     * @throws BeansException
+     */
+    private Object initializeBean(String beanName, Object bean, BeanDefinition beanDefinition) throws BeansException{
+        // 1.执行BeanPostProcessor 前置处理
+        Object wrappedBean = applyBeanPostProcessorsBeforeInitialization(bean, beanName);
+
+        // 2.执行init方法
+        try{
+            invokeInitMethods(beanName, wrappedBean, beanDefinition);
+        } catch (Exception e){
+            throw new BeansException("Invocation of init method of bean [" + beanName +"] failed", e);
+        }
+
+        // 3. 执行Bean对象的初始化方法
+        wrappedBean = applyBeanPostProcessorsAfterInitialization(wrappedBean, beanName);
+        return wrappedBean;
+    }
+
+    /**
+     * 调用 bean 的初始化方法
+     * @param beanName
+     * @param bean
+     * @param beanDefinition
+     * @throws Exception
+     */
+    private void invokeInitMethods(String beanName, Object bean, BeanDefinition beanDefinition) throws Exception {
+        // 1. 执行接口initializingBean 的方法afterPropertiesSet
+        if (bean instanceof InitializingBean){
+            ((InitializingBean) bean).afterPropertiesSet();
+        }
+
+        String initMethodName = beanDefinition.getInitMethodName();
+        if(StrUtil.isNotEmpty(initMethodName) && !(bean instanceof  InitializingBean)){
+            Method initMethod = beanDefinition.getBeanClass().getMethod(initMethodName);
+            if (null == initMethod) {
+                throw new BeansException("Could not find an init method named '" + initMethodName +"' on bean with name [" + beanName + "]");
+            }
+            initMethod.invoke(bean);
+        }
+
+    }
+
+    @Override
+    public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor processor : getBeanPostProcessors()) {
+            Object current = processor.postProcessBeforeInitialization(existingBean, beanName);
+            if(null == current) return result;
+            result = current;
+        }
+        return result;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor processor : getBeanPostProcessors()) {
+            Object current = processor.postProcessAfterInitialization(existingBean, beanName);
+            if(null == current) return result;
+            result = current;
+        }
+        return result;
+    }
 }
