67a82704370bebd87505676d062f3eb38563bb61
jianbo <1784749609@qq.com>
2024-07-21 16:36:33 +0800

    实现Bean工厂的创建

---
+++ src/main/java/com/tiny/springframework/context/AbstractRefreshableApplicationContext.java
@@ -0,0 +1,30 @@
+package com.tiny.springframework.context;
+
+import com.tiny.springframework.bean.factory.ConfigurableListableBeanFactory;
+import com.tiny.springframework.bean.exception.BeansException;
+import com.tiny.springframework.bean.factory.support.DefaultListableBeanFactory;
+
+/**
+ * 获取Bean工厂并加载资源
+ */
+public abstract class AbstractRefreshableApplicationContext extends AbstractApplicationContext{
+    private DefaultListableBeanFactory beanFactory;
+
+    @Override
+    protected void refreshBeanFactory() throws BeansException {
+        DefaultListableBeanFactory beanFactory = createBeanFactory();
+        loadBeanDefinition(beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    private DefaultListableBeanFactory createBeanFactory(){
+        return new DefaultListableBeanFactory();
+    }
+
+    protected abstract void loadBeanDefinition(DefaultListableBeanFactory beanFactory) throws BeansException;
+
+    @Override
+    protected ConfigurableListableBeanFactory getBeanFactory() {
+        return this.beanFactory;
+    }
+}
