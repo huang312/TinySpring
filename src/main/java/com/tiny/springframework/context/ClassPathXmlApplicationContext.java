88ba319a4bf7dd846387d4290f8e4738fb098e42
jianbo <1784749609@qq.com>
2024-07-21 16:39:16 +0800

    基于classpath的应用上下文实现类

---
+++ src/main/java/com/tiny/springframework/context/ClassPathXmlApplicationContext.java
@@ -0,0 +1,39 @@
+package com.tiny.springframework.context;
+
+import com.tiny.springframework.bean.exception.BeansException;
+
+/**
+ * 基于classpath的应用上下文实现类
+ */
+public class ClassPathXmlApplicationContext extends AbstractXmlApplicationContext{
+
+    private String[] configLocations;
+
+    public ClassPathXmlApplicationContext(){}
+
+    /**
+     * 从XML中加载BeanDefinition并刷新上下文
+     *
+     * @param configLocations
+     * @throws BeansException
+     */
+    public ClassPathXmlApplicationContext(String configLocations) throws BeansException{
+        this(new String[]{configLocations});
+    }
+
+    /**
+     * 从XML中加载BeanDefinition 并刷新上下文
+     *
+     * @param configLocations
+     * @throws BeansException
+     */
+    public ClassPathXmlApplicationContext(String[] configLocations) throws BeansException{
+        this.configLocations = configLocations;
+        refresh();
+    }
+
+    @Override
+    protected String[] getConfigLocations() {
+        return configLocations;
+    }
+}
