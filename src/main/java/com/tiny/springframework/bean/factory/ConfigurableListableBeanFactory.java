d5e2236c8628de338cda71790edeadd4fa2759df
jianbo <1784749609@qq.com>
2024-07-21 16:17:52 +0800

    实现ConfigurableListableBeanFactory中的提前实例化方法

--- src/main/java/com/tiny/springframework/bean/factory/ConfigurableListableBeanFactory.java
+++ src/main/java/com/tiny/springframework/bean/factory/ConfigurableListableBeanFactory.java
@@ -1,5 +1,6 @@
 package com.tiny.springframework.bean.factory;
 
+import com.tiny.springframework.bean.exception.BeansException;
 import com.tiny.springframework.bean.factory.config.AutowireCapableBeanFactory;
 import com.tiny.springframework.bean.factory.config.ConfigurableBeanFactory;
 
@@ -7,5 +8,7 @@ import com.tiny.springframework.bean.factory.config.ConfigurableBeanFactory;
  * 提供分析和修改Bean以及余弦实例化的操作接口
  */
 public interface ConfigurableListableBeanFactory extends ConfigurableBeanFactory, ListableBeanFactory, AutowireCapableBeanFactory {
+
+    void preInstantiateSingletons() throws BeansException;
 
 }
--- src/main/java/com/tiny/springframework/bean/factory/support/DefaultListableBeanFactory.java
+++ src/main/java/com/tiny/springframework/bean/factory/support/DefaultListableBeanFactory.java
@@ -1,8 +1,10 @@
 package com.tiny.springframework.bean.factory.support;
 
+import com.tiny.springframework.bean.factory.ConfigurableListableBeanFactory;
 import com.tiny.springframework.bean.factory.config.BeanDefinition;
 import com.tiny.springframework.bean.exception.BeansException;
 
+import java.beans.Beans;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +12,7 @@ import java.util.Map;
  * 默认的 BeanFactory 实现类，实现了BeanFactory的完整功能
  * 包括 BeanDefinition 注册器，Bean 注册器以及 Bean 的获取功能
  */
-public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry{
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry, ConfigurableListableBeanFactory {
     // 保存 BeanDefinition
     private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
@@ -29,5 +31,29 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
     @Override
     public boolean containsBeanDefinition(String beanName) {
         return beanDefinitionMap.containsKey(beanName);
+    }
+
+    @Override
+    public void preInstantiateSingletons() throws BeansException {
+        for (String beanName : beanDefinitionMap.keySet()) {
+            getBean(beanName);
+        }
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        Map<String, T> result = new HashMap<>();
+        for (String beanName : beanDefinitionMap.keySet()) {
+            Class beanClass = beanDefinitionMap.get(beanName).getBeanClass();
+            if (type.isAssignableFrom(beanClass)) {
+                result.put(beanName, (T) getBean(beanName));
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String[] getBeanDefinitionNames() {
+        return beanDefinitionMap.keySet().toArray(new String[0]);
     }
 }
